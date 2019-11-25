a310be0abfe15599fe4f4d02dbb3a2ce41f8332d
pierre <nanashi4781@gmail.com>
2019-11-25 16:35:41 +0100

    yep

--- view/coach.php
+++ view/coach.php
@@ -3,8 +3,14 @@
 <div class="container">
     <div class="row">
         <?php foreach ($coachs as $index => $pres) { ?>
+            
             <div class="col-md-4 mt-3 ">
-                <?php echo $pres['name']; ?>
+                <div class="card bg-light">
+                    <img class="card-img-top" src="<?php echo $team['logo']; ?>" alt="logo de l'equipe">
+                  <div class="card-body">
+                    <h5 class="card-title text-center"><?php echo $pres['name']; ?></h5>
+                  </div>
+              </div>
             </div>
         <?php } ?>
     </div>
