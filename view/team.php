16a30b73732583657ffbf8368de4c75f96f42ff9
pierre <nanashi4781@gmail.com>
2019-11-26 16:41:24 +0100

    finishplayer

--- view/team.php
+++ view/team.php
@@ -1,7 +1,7 @@
 <?php ob_start(); ?>
 
 <div class="container">
-    <div class="card bg-dark mt-3">
+    <div class="card test1 bg-dark mt-3">
         <div class="row">
             <div class="col-md-6 text-center">
                 <img class="card-img-top4" src="<?php echo $team['logo']; ?>" alt="logo de l'equipe">
@@ -17,14 +17,21 @@
 </div>
 <div class="container">
     <div class="row">
+        <?php
+        function taille ($sizeEnCm) {
+            $resultat = $sizeEnCm/100;
+            return $resultat . ' m';
+        }
+        ?>
         <?php foreach ($players as $index => $player) { ?>
             <div class="col-md-6 mt-3 ">
-                <div class="card bg-dark text-white">
+                <div class="card test bg-dark text-white">
                     <div class="text-center mt-2 mb-2">
                     <p><?php echo $player['short_name']; ?></p>
                     <img class="card-img-top mb-3" src="<?php echo $player['photo']; ?>"alt="photo du joueur">
                     <p>Date de naissance : <?php echo (new dateTime($player['birthday_date']))->format('d/m/y');?></p>
                     <p>Lieu de naissance : <?php echo $player['birthday_place']; ?></p>
+                    <p>Taille : <?php echo taille($player['size']); ?></p>
                     <p>Nationalité : <?php echo $player['nationality']; ?></p>
                     </div>
                   </div>
